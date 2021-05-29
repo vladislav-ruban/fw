223fc5eb688f2ae6de6febd6be9e62ba6e03a46a
Vladislav <vladislav.ruban@tech-stack.io>
2021-05-29 16:32:18 +0300

    moved dependency and used version variable in pom and changed feature path in runner

--- src/test/java/RunBddTests.java
+++ src/test/java/RunBddTests.java
@@ -1,6 +1,6 @@
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = {"src/test/java/Features/SearchTest.feature"}, plugin = {"pretty"})
+@CucumberOptions(features = {"src/test/java/Features"}, plugin = {"pretty"})
 public class RunBddTests extends AbstractTestNGCucumberTests {
 }
