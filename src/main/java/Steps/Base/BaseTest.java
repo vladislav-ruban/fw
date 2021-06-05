9a31456153eeb59811791039928b329413fae1a8
Vladislav <vladislav.ruban@tech-stack.io>
2021-06-05 23:16:36 +0300

    removed comments

--- src/main/java/Steps/Base/BaseTest.java
+++ src/main/java/Steps/Base/BaseTest.java
@@ -37,42 +37,4 @@ public class BaseTest extends BaseUtil {
         base.getDriver()
             .close();
     }
-
-
-/*
-    @After(order = 1)
-    public void embedScreenshot(Scenario scenario) {
-        if (scenario.isFailed()) {
-            Calendar calendar = Calendar.getInstance();
-            SimpleDateFormat formater = new SimpleDateFormat("dd_MM_yyyy_hh_mm_ss");
-            String methodName = scenario.getName();
-
-            File scrFile = ((TakesScreenshot) base.getDriver()).getScreenshotAs(OutputType.FILE);
-            String screenshotName = String.format("%s_%s", methodName, formater.format(calendar.getTime()));
-            try {
-                FileUtils.copyFile(scrFile, new File(String.format("target\\allure-results\\screenshots\\%s",
-                        screenshotName)));
-            } catch (IOException e1) {
-                e1.printStackTrace();
-            }
-        }
-    }
-*/
-/*
-    @AfterMethod(alwaysRun=true)
-    public void catchExceptions(ITestResult result){
-        Calendar calendar = Calendar.getInstance();
-        SimpleDateFormat formater = new SimpleDateFormat("dd_MM_yyyy_hh_mm_ss");
-        String methodName = result.getName();
-        if(!result.isSuccess()){
-            File scrFile = ((TakesScreenshot) base.getDriver()).getScreenshotAs(OutputType.FILE);
-            String screenshotName = String.format("%s_%s.png", methodName, formater.format(calendar.getTime()));
-            try {
-                FileUtils.copyFile(scrFile, new File(String.format("target\\allure-results\\screenshots\\%s",
-                screenshotName)));
-            } catch (IOException e1) {
-                e1.printStackTrace();
-            }
-        }
-    }*/
 }
