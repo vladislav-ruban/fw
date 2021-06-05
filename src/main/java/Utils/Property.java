c079c6ec93f5c7ea323f902591859587514cbec7
Vladislav <vladislav.ruban@tech-stack.io>
2021-06-05 19:12:23 +0300

    formatted

--- src/main/java/Utils/Property.java
+++ src/main/java/Utils/Property.java
@@ -6,11 +6,12 @@ import java.util.Properties;
 
 public class Property {
     private static Properties properties = null;
+
     public static String getProperty(String key) {
         try {
             properties = new Properties();
             properties.load(Objects.requireNonNull(Property.class.getClassLoader()
-                    .getResourceAsStream("configuration.properties")));
+                                                                 .getResourceAsStream("configuration.properties")));
             return properties == null ? null : properties.getProperty(key, "");
         } catch (IOException e) {
             throw new IllegalStateException(e);
