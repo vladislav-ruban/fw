dafe3154aeb5f1083dd477f18ae75e5f37cbe3c9
Vladislav <vladislav.ruban@tech-stack.io>
2021-06-05 22:31:01 +0300

    added logic to pass bot check

--- src/main/java/Steps/Base/BaseUtil.java
+++ src/main/java/Steps/Base/BaseUtil.java
@@ -1,6 +1,7 @@
 package Steps.Base;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
 
 import Utils.Property;
 
@@ -8,11 +9,20 @@ public class BaseUtil {
 
     private WebDriver driver;
 
-    public WebDriver getDriver() { return driver; }
+    public WebDriver getDriver() {
+        return driver;
+    }
 
-    public void setDriver(WebDriver driver) { this.driver = driver;}
+    public void setDriver(WebDriver driver) {
+        this.driver = driver;
+    }
 
     public void openMainPage() {
+        Actions action = new Actions(driver);
+        action.moveByOffset(5, 5)
+              .moveByOffset(10, 15)
+              .moveByOffset(20, 15);
         getDriver().get(Property.getProperty("mainPage"));
+        action.perform();
     }
 }
